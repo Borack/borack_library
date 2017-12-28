908372fd27302c5e892b018bf0eec327ed1ef46d
Borack <borack@gmail.com>
2017-12-28 16:21:37 +0100

    initial cmake setup

---
+++ app/app.cmake
@@ -0,0 +1,8 @@
+set(APP_DIR ${root}/app)
+
+set(sources
+ ${APP_DIR}/main.cpp)
+ 
+include_directories(BEFORE SYSTEM ${LIB_API_INCLUDE})
+add_executable(${PROJECT_NAME} ${sources})
+target_link_libraries(${PROJECT_NAME} ${LIBS})
---
+++ library/library.cmake
@@ -0,0 +1,11 @@
+set(LIB_DIR ${root}/library)
+
+set(LIBRARY_API_INCLUDE
+        ${LIB_DIR}/include/api.hpp PARENT_SCOPE)
+	
+set(lib_sources
+  ${LIB_DIR}/src/calculator.cpp
+  )
+
+include_directories(BEFORE SYSTEM ${LIBRARY_API_INCLUDE})  
+add_library(LIBS ${lib_sources})
